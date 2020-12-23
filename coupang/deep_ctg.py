1cc3566e43913a1293e9a60caec4e591c7560b05
ssamko <ssamko@gdflab.com>
2020-12-24 07:03:10 +0900

    lv4 5url

--- coupang/deep_ctg.py
+++ coupang/deep_ctg.py
@@ -12,7 +12,7 @@ options = Options()
 options.page_load_strategy = 'eager'
 # driver = webdriver.Chrome(options=options,executable_path="/Users/ssamko/Downloads/chromedriver")
 
-# url = 'https://www.coupang.com'
+coupang_url = 'https://www.coupang.com'
 headers = {
            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:38.0) Gecko/20100101 Firefox/38.0',
            'Content-Type': 'text/html; charset=utf-8'
@@ -32,7 +32,7 @@ hdr =  {
 
 }
 
-with open(os.path.join(BASE_DIR, 'ctg_url_v2.txt'), 'r') as f:
+with open(os.path.join(BASE_DIR, 'ctg_url_v3.txt'), 'r') as f:
     data = f.readlines()
 
 for row in data[4:]:
@@ -46,8 +46,14 @@ for row in data[4:]:
         if lis:
             for li in lis:
                 label = li.select_one('label')
-                print(ctg3+'$'+label.text)
+                print(ctg3+'$'+label.text, end='')
+                a = li.select_one('a')
+                if a:
+                    print('%'+coupang_url+li['data-link-uri'])
+                else:
+                    print()
             break
+
 
 
 # for row in data:
